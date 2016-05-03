d7fe174edf06f049eaaa8a413dbefac3787db3d0
Kyle Bradshaw <kyle.bradshaw@digitas.com>
2016-05-03 13:41:59 -0400

    components in components

--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -1,11 +1,14 @@
 import {Component} from 'angular2/core';
+import {ContactHeaderComponent} from './contact-header-component/contact-header-component';
 
 @Component ({
   selector: 'contacts-app',
   styleUrls: ['app/contacts-app.css'],
+  directives: [ContactHeaderComponent],
   template: `
+    <contact-header-component></contact-header-component>
     <h1>Hello World</h1>
-  `  
+  `
 })
 
 export class ContactsApp {
