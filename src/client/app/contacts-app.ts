d95461dae1e7e5ce25faa4e68db657a13cc048e7
Kyle Bradshaw <kyle.bradshaw@digitas.com>
2016-05-03 13:05:30 -0400

    first componenent created

--- src/client/app/contacts-app.ts
+++ src/client/app/contacts-app.ts
@@ -1,1 +1,13 @@
 import {Component} from 'angular2/core';
+
+@Component ({
+  selector: 'contacts-app',
+  styleUrls: ['app/contacts-app.css'],
+  template: `
+    <h1>Hello World</h1>
+  `  
+})
+
+export class ContactsApp {
+  
+}
